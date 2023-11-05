c652dc632e99c875ebc6c2f51c1fdf315ec450c4
hieu.pham2904 <21520857@gm.uit.edu.vn>
2023-11-05 19:35:02 +0700

    .

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,5 @@
 const user = require('../models/userModel')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const { generalAccessToken, generalRefreshToken } = require('./JwtService')
 
 const createUser = (newUser) => {
